bb4f40fc16b8ebf6b8a976b3b3f795f83be489c5
Michael L Parks <github@wmi-inc.com>
2016-06-09 19:34:35 -0400

    Added Slide Feature To Minimize Scrolling By User

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 
 var $startGameButton = $("#bt-start");
 var $playingArea = $("#play-area");
-
+var $banner = $("#banner");
 var $memoryCard = $(".card img");
 
 var cardImages = ["img1.jpg",
@@ -35,6 +35,10 @@ var outputBoard = "";
 
 var card1;
 var card2;
+
+var possibleMatches;
+var matchedCards;
+
 // _ Play game until all matches are found (UNTIMED)
 
 
@@ -67,19 +71,49 @@ function flipCard(){
     $(this).toggleClass("showImage");
     var cardImage = $(this).find("img").attr("src");
     cardsFlipped.push(cardImage);
+
     if(cardsFlipped.length == 3){
       if(cardsFlipped[0] == cardsFlipped[1] && cardsFlipped.length){
           $(this).siblings(".showImage").addClass("match");
+          alert("It's a MATCH!!!");
+          //Check for gameOver()
+          if(gameOver()){
+            cardsFlipped = [];
+            alert("Click START to begin new game...");
+          }
       }
       $(this).siblings(".showImage").removeClass("showImage");
       cardsFlipped = [cardsFlipped[2]];
     }
 }
 
+function gameOver(){
+  //Game is over when all cards are matched
+  //To find the number of possible matches, take the length of the cardImages
+      //Array and compare to the number of cards that have the class MATCH
+      matchedCards = numberOfMatchedCards();
+      if(matchedCards == cardImages.length){
+        alert("***** Game Over *****");
+        return true;
+      }else{
+        return false;
+      }
+}
+
+function numberOfMatchedCards(){
+    matchedCards = $(".match").length;
+    console.log(matchedCards);
+    return matchedCards;
+}
+// $("button").hasClass("match ");
+
+
+
+
+
 $(document).ready(function(){
-
       $startGameButton.on("click",function(){
-
+              $banner.slideUp();
               buildBoard();
               var $playingAreaDiv = $("#play-area > div");
               $(".card img").css("display","none");
